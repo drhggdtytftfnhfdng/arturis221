52cd11839a7f44f64cceb67ad10dd3840ba9d996
drhggdtytftfnhfdng <117073262+drhggdtytftfnhfdng@users.noreply.github.com>
2023-01-18 18:52:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+// 1
+// Переменные:myDog, years ,guests 
+// Встроенные функции:console.log,
+// Функции:makeTea ,secret
+// Аргументы:(guests, 'Earl Grey');(myDog, 4);
+// Параметры:(dogname, age),(cups, tea)
+
+
+
+// 2
+// let txt = 'JS';
+// console.log(txt.toLowerCase());
+
+// 3
+// let txt = 'я люблю JS!';
+// console.log(txt.substr(0,1));
+// console.log(txt.substring(0,1));
+// console.log(txt.slice(0,1));
+
+// 4
+// let txt = 'я люблю JS!';
+// console.log(txt.indexOf("люблю"));
+
+// 5
+// let txt = 'конец';
+// let n = 4
+// let result = 0
+// if(n<txt.length){
+//     result = txt.substr(0,6);
+//     console.log(result + '...');
+// }else{
+//     result=txt
+//     console.log(result)
+// }
+
+
+// 6
+// let txt = 'Я-люблю-JS!';
+// console.log(txt.replace(/-/g, '!'))
+
+// 7
+// let name = 'я люблю JS';
+// console.log(name.split(""))
+
+// 8
+// let name = 'привет мир';
+// console.log(name.split(""))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
